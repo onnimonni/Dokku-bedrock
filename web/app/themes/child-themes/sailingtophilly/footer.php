d79bf9a2b13608d393d7192175af3010ddc1d77b
Onni Hakala <hakala.onni@gmail.com>
2014-04-08 12:33:29 +0300

    added koodimonni logo to footer

--- web/app/themes/child-themes/sailingtophilly/footer.php
+++ web/app/themes/child-themes/sailingtophilly/footer.php
@@ -15,7 +15,8 @@ $bavotasan_theme_options = bavotasan_theme_options();
 			<div class="row">
 				<div class="copyright col-lg-12">
 					<span class="pull-left"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'arcade' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
-					<span class="credit-link pull-right"><i class="fa fa-leaf"></i><?php printf( __( 'Powered by %s.', 'arcade' ), '<a href="http://koodimonni.fi">Koodimonni</a>' ); ?></span>
+					<a href="http://koodimonni.fi"><img src="http://koodimonni.fi/logo_gray.svg" id="koodimonni_logo"></img></a>
+					<span class="credit-link pull-right">Powered by<br><?php printf( __( '%s', 'arcade' ), '<a href="http://koodimonni.fi">Koodimonni</a>' ); ?></span>
 				</div><!-- .col-lg-12 -->
 			</div><!-- .row -->
 		</div><!-- #footer-content.container -->
